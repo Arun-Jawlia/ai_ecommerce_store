4a97ebf5ef998c14741917d4edd6e3975be6029f
Arun-Jawlia <arunkumar08.mk@gmail.com>
2026-01-14 22:11:27 +0530

    create(AI_POWERED_ECOMMERCE_STORE): basic folder structure with node, express and postgresql

---
+++ server/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+
+const app = express();
+config({ path: "./config/config.env" });
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    tempFileDir: "./uploads",
+    useTempFiles: true,
+  })
+);
+
+export default app;
